0efc925ce7277cc77b6c16cde861b8c67eb94411
Morsapir <92923336+Morsapir@users.noreply.github.com>
2023-12-13 13:36:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ $('.code-editor').on('input', function () {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
